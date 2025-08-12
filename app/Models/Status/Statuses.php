e6c2b758dafabf5f34c4d654a764877827f3347e
unknown <pandaaa2984@gmail.com>
2025-08-12 13:24:42 +0700

    commit

---
+++ app/Models/Status/Statuses.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Status;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Statuses extends Model
+{
+    use SoftDeletes;
+    protected $table = 'statuses';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nama',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+}
